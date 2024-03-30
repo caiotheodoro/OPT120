0b215d5c943df882e09723eb449fd7b519d768a1
caiotheodoro <caio.theodoro@mblabs.com.br>
2024-03-29 22:52:41 -0300

    feat: 🎸 general

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,29 +1,39 @@
-import express, {Application, Request, Response, NextFunction} from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import {pool,initalize} from './database'
-const app = express()
-app.use(cors())
-app.use(bodyParser.json())
+import express, { Application, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { pool, initalize } from "./database";
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 initalize();
-app.get('/', async (req, res) => {
-    res.send('Heylo') 
-    await pool.query('SELECT * FROM USER')
-    .then(res => console.log(res))
-    .catch(err => console.error(err))
-    
-})
-
-app.post('/api/post', async (req, res) => {
-    await pool.query('INSERT INTO "user" (name, email, password) VALUES ($1, $2, $3)', [req.body.name, req.body.email, req.body.password])
-    
-    res.status(201).send('Usuario adicionado com sucesso');
-})
-
-app.get('/api/get/:id', async (req, res) => {
-    const response = await pool.query('SELECT * FROM "user" WHERE id = $1', [req.params.id]);
-    console.log(response);
-    res.status(200).send(response.rows);
+app.get("/", async (req, res) => {
+  res.send("Heylo");
+});
+app.get("/users", async (req, res) => {
+  const response = await pool.query('SELECT id, name, email FROM "user"');
+  console.log(response.rows);
+  //send response body and status code
+  res.status(200).send(response.rows);
 });
 
-app.listen(3025, () => {console.log('Listening to 3025')});
+app.post("/users", async (req, res) => {
+  console.log(req.body);
+  await pool.query(
+    'INSERT INTO "user" (name, email, password) VALUES ($1, $2, $3)',
+    [req.body.name, req.body.email, req.body.password],
+  );
+
+  res.status(201).send("Usuario adicionado com sucesso");
+});
+
+app.get("/api/get/:id", async (req, res) => {
+  const response = await pool.query('SELECT * FROM "user" WHERE id = $1', [
+    req.params.id,
+  ]);
+  console.log(response);
+  res.status(200).send(response.rows);
+});
+
+app.listen(3025, () => {
+  console.log("Listening to 3025");
+});
